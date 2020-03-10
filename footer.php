41d7ad39059ceff3beb2bae396c03f881ad1bbe2
AliciaLH <44652262+AliciaLH@users.noreply.github.com>
2020-03-10 13:24:21 -0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer>
+        <p>All artists are credited <a href="artists.html">here</a></p>
+        <p>Have any countries or cities that you wanted to see? <a href="about.html">Contact Us!</a></p>
+    </footer>
